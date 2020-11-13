81636ec7788f166c0d22ed6e4b7d29ecf4053f4c
nkleinmann <nekleinmann@gmail.com>
2020-11-12 22:57:44 -0500

    added schema in models folder

--- client/src/components/search/search.jsx
+++ client/src/components/search/search.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import "./search.css";
 
+// Function to allow user to search employees
+// handleInputChange = (event) => {
+//     // let value = event.target.value;
+//     // const name = event.target.name;
+//     console.log(value);
+//     // Set the state to input
+//     // this.setState({
+//     //     [name]: value
+//     // }, () => {
+//     //     if (this.state.search) {
+//     //         let searchedEmps = filter.searchEmployees(this.state.search, this.state.currentSearchArray);
+//     //         this.setState({
+//     //             employees: searchedEmps,
+//     //             orderBy: ""
+//     //         })
+//     //     } else {
+//     //         this.setState({
+//     //             employees: this.state.currentSearchArray,
+//     //             orderBy: ""
+//     //         })
+//     //     }
+//     // });
+// };
+
 const Search = (props) => {
     return (
         <div className="search card text-class">
             <div className="row justify-content-center">
                 <form className="col-8">
                     <input type="text" class="form-control m-2" id="exampleBook" aria-describedby="bookHelp" placeholder="Enter book" />
+                    {/* handleInputChange={this.handleInputChange} */}
                 </form>
                 <button type="submit" class="btn btn-primary col-3 m-2">Submit</button>
             </div>
 
-            </div>
+        </div>
     )
 }
 
---
+++ models/books.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema(
+  {
+    
+    authors: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    image: {
+      type: String
+    },
+    link: {
+      type: String
+    },
+    title: {
+      type: String,
+      // trim: true,
+      required: true
+    }
+  }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
