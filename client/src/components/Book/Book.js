65f0bbffd7bd3c2ffcf91384278e109bcf53ff27
nkleinmann <nekleinmann@gmail.com>
2020-11-14 15:33:16 -0500

    image works for books

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -21,16 +21,21 @@ const Book = ({ volumeInfo }) => {
     return (
         <div className="col-12">
             <div className="card">
+                <div className="row justify-content-center">
+                <button className="save-btn col-3 m-3" onClick={handleBooksubmit}>Save</button>
+            <button className="view-btn col-3 m-3"><a href={volumeInfo.infoLink} target="_blank">View</a></button>
+                </div>
+           
                 <p>Title: {volumeInfo.title}</p>
                 <p>Author(s): {volumeInfo.authors}</p>
                 <p>Description: {volumeInfo.description || "No description available"}</p>
                 <div>
-                {/* <img src= {volumeInfo.imageLinks.thumbnail || "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png"} alt="book cover" /> */}
+                <img src= {volumeInfo.imageLinks?.thumbnail || "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/100px-No_image_available.svg.png"} alt="book cover" />
                 </div>
-                {/* <p>Link: <a href={volumeInfo.infoLink} target="_blank">{volumeInfo.title}</a> </p> */}
+                <p>Link: <a href={volumeInfo.infoLink} target="_blank">{volumeInfo.title}</a> </p>
+                
             </div>
-            <button className="save-btn" onClick={handleBooksubmit}>Save</button>
-            <button className="view-btn"><a href={volumeInfo.infoLink} target="_blank">View</a></button>
+            
         </div>
     )
 }
