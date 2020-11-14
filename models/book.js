b92d0fdc598c0671856634a9afbea20e9f8b91c2
nkleinmann <nekleinmann@gmail.com>
2020-11-13 20:31:38 -0500

    adding routes, controllers, and models

--- models/book.js
+++ models/book.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema(
-  {
-    
+  {  
     authors: {
       type: String
     },
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,10 +15,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Define API routes here
+app.use(routes);
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-
-// Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
